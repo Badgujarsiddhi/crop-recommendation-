121371675c26c5c2f317d305cb5ee5f13bdfbaf1
BadgujarSiddhi <siddhibadgujar18@gmail.com>
2026-01-01 11:55:35 +0530

    fixing traning model

--- ui/train_model.py
+++ ui/train_model.py
@@ -2,15 +2,20 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import pickle
+import os
 
-# Load dataset
-data = pd.read_csv("Crop_recommendation.csv")
 
-# Features & Label
+# Load data
+data = pd.read_csv(r"C:\Users\siddh\OneDrive\Desktop\Documents\croprecom\crop-recommendation-\ui\Crop_recommendation.csv")
+
+# Check columns
+print("Columns:", data.columns)
+
+# TARGET COLUMN IS 'label'
 X = data.drop("label", axis=1)
 y = data["label"]
 
-# Split data
+# Train-test split
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
